e7679a7976c68301800a9145afc2210140a11b1d
iamhassankhan <iamhassan101@gamil.com>
2023-03-07 14:44:12 +0500

    Flex box 3/4

--- newApp/App.js
+++ newApp/App.js
@@ -1,11 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text, Platform, StatusBar } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.row}>
+        <View style={styles.box}>
+          <Text style={styles.text}>Hi</Text>
+        </View>
+        <View style={styles.box}>
+          <Text style={styles.text}>Hi</Text>
+        </View>
+        <View style={styles.box}>
+          <Text style={styles.text}>Hi</Text>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.box}>
+          <Text style={styles.text}>Hi</Text>
+        </View>
+        <View style={styles.box}>
+          <Text style={styles.text}>Hi</Text>
+        </View>
+        <View style={styles.box}>
+          <Text style={styles.text}>Hi</Text>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.box}>
+          <Text style={styles.text}>Hi</Text>
+        </View>
+        <View style={styles.box}>
+          <Text style={styles.text}>Hi</Text>
+        </View>
+        <View style={styles.box}>
+          <Text style={styles.text}>Hi</Text>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.box}>
+          <Text style={styles.text}>Hi</Text>
+        </View>
+        <View style={styles.box}>
+          <Text style={styles.text}>Hi</Text>
+        </View>
+        <View style={styles.box}>
+          <Text style={styles.text}>Hi</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +55,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : null,
+  },
+  row: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  box: {
+    flex: 1,
+    margin: 5,
+    backgroundColor: "tomato",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 30,
+    color: "white",
   },
 });
